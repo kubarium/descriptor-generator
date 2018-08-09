82e7ca521ef5a79586618f4cf8e92032c5685d33
kubarium <iam@denizkumsal.com>
2018-08-09 01:16:13 -0400

    adds Bulma, removes bootstrap, new layout, adds swap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css'
 
-import {Button, Col, ControlLabel, FormControl, FormGroup, Grid, Navbar, Row, Well} from 'react-bootstrap'
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import classNames from "classnames"
+import ReactNbsp from "react-nbsp"
 
 class App extends Component {
   constructor(props) {
@@ -9,68 +10,109 @@ class App extends Component {
     this.state = {
       bits: localStorage.getItem("descriptorBits") || "",
       pieces: localStorage.getItem("descriptorPieces") || "",
-      bits_and_pieces:""
+      bits_and_pieces: ""
     }
   }
   generate = () => {
-    let bits = this.state.bits.split("\n").filter(entry=>entry!=="")
-    let pieces = this.state.pieces.split("\n").filter(entry=>entry!=="")
+    let bits = this
+      .state
+      .bits
+      .split("\n")
+      .filter(entry => entry !== "")
+    let pieces = this
+      .state
+      .pieces
+      .split("\n")
+      .filter(entry => entry !== "")
 
     this.setState({
-      bits_and_pieces:`${bits[Math.floor(Math.random() * bits.length)]} ${pieces[Math.floor(Math.random() * pieces.length)]}`
+      bits_and_pieces: `${bits[Math.floor(Math.random() * bits.length)]} ${pieces[Math.floor(Math.random() * pieces.length)]}`
     })
   }
   saveBits = (event) => {
-    this.setState({
-      bits:event.target.value
-    })
+    this.setState({bits: event.target.value})
     localStorage.setItem("descriptorBits", event.target.value)
   }
   savePieces = (event) => {
-    this.setState({
-      pieces:event.target.value
-    })
+    this.setState({pieces: event.target.value})
     localStorage.setItem("descriptorPieces", event.target.value)
+  }
+  swap = (event) => {
+    const bits = this.state.bits
+    const pieces = this.state.pieces
+
+    this.setState({pieces: bits, bits: pieces})
+    localStorage.setItem("descriptorBits", pieces)
+    localStorage.setItem("descriptorPieces", bits)
   }
   render() {
     return (
       <main>
-        <Navbar inverse>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <h1>Descriptor Generator</h1>
-            </Navbar.Brand>
-            <Navbar.Toggle/>
-          </Navbar.Header>
-        </Navbar>
-        <Grid>
-          <Row className="show-grid">
-            <Col xs={6} md={4}>
-              <FormGroup>
-                <ControlLabel>Bits</ControlLabel>
-                <FormControl componentClass="textarea" onChange={this.saveBits} rows="20" defaultValue={this.state.bits} placeholder="put your bits here, hit enter between each entry"/>
-              </FormGroup>
-            </Col>
-            <Col xs={6} md={4}>
-              <FormGroup>
-                <ControlLabel>Pieces</ControlLabel>
-                <FormControl componentClass="textarea" onChange={this.savePieces} rows="20" defaultValue={this.state.pieces} placeholder="put your pieces here, hit enter between each entry"/>
-              </FormGroup>
-            </Col>
-            <Col xsHidden md={4}>
-              <FormGroup>
-                <ControlLabel>Instructions</ControlLabel>
-                <ul>
-                  <li>Insert text into relevant fields</li>
-                  <li>Separate entries by pressing return</li>
-                  <li>Generate a descriptor</li>
-                </ul>
-                <Button bsStyle="primary" onClick={this.generate}>Generate</Button>
-              </FormGroup>
-              <Well hidden={this.state.bits_and_pieces.length === 0}>{this.state.bits_and_pieces}</Well>
-            </Col>
-          </Row>
-        </Grid>
+        <div className="section">
+          <h1 className="title is-4 has-text-centered">
+            Descriptor Generator
+          </h1>
+        </div>
+        <div className="container">
+          <div className="section">
+            <div className="columns">
+              <div className="column">
+                <div className="content">
+                  <h2 className="subtitle is-5">
+                    Instructions
+                  </h2>
+                  <ul>
+                    <li>Fill fields with text by pressing
+                      <ReactNbsp/>
+                      <span className="tag is-dark">Return</span><ReactNbsp/>
+                      to separate entries</li>
+                    <li>Alternatively, swap the content of the fields</li>
+                  </ul>
+                </div>
+              </div>
+              <div
+                className="column is-half-mobile is-two-fifths-tablet is-one-third-desktop">
+                <div className="level">
+                  <div className="level-left">
+                    <button className="button is-info is-fullwidth" onClick={this.swap}>Swap Bits&Pieces</button>
+                  </div>
+                  <div className="level-right">
+                    <button className="button is-primary is-fullwidth" onClick={this.generate}>Generate Descriptor</button>
+                  </div>
+                </div>
+                <div
+                  className={classNames('notification', {
+                  hidden: this.state.bits_and_pieces.length === 0
+                })}>
+                  {this.state.bits_and_pieces}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="section">
+            <div className="columns">
+              <div className="column">
+                <label className="label">Bits</label>
+                <textarea
+                  className="textarea"
+                  onChange={this.saveBits}
+                  rows="20"
+                  value={this.state.bits}
+                  placeholder="put your bits here, hit enter between each entry"></textarea>
+              </div>
+              <div className="column">
+                <label className="label">Pieces</label>
+                <textarea
+                  className="textarea"
+                  onChange={this.savePieces}
+                  rows="20"
+                  value={this.state.pieces}
+                  placeholder="put your pieces here, hit enter between each entry"></textarea>
+              </div>
+            </div>
+          </div>
+
+        </div>
       </main>
     );
   }
